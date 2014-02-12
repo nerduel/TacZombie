892131c36a825fd76b8a72f1fd5bf77d5e4cadf3
Manuel Hieke <mahieke@htwg-konstanz.de>
2014-02-12 16:30:59 +0100

    quick fix getValidMoves

--- model/src/main/scala/taczombie/model/util/CoordinateHelper.scala
+++ model/src/main/scala/taczombie/model/util/CoordinateHelper.scala
@@ -2,6 +2,9 @@ package taczombie.model.util
 
 import scala.collection.mutable.ListBuffer
 import scala.language.implicitConversions
+import taczombie.model.Game
+import taczombie.model.GameFieldCell
+import taczombie.model.GameField
 
 object CoordinateHelper {
 
@@ -37,62 +40,69 @@ object CoordinateHelper {
     	tuple._2 > rVal._2
     }
     
-    def calculateAllowedMoves(range : Int) = {
+    // TODO: why is that in this class???
+    // How should that work without accessing the gameField?????
+    def calculateAllowedMoves(range : Int, g : Game) = {
       var toVisit = scala.collection.mutable.Stack.empty[(Int,Int)]
       var pathList = ListBuffer(ListBuffer.empty[(Int, Int)])
       var element = tuple
       var col = 0
       var alreadyVisited = ListBuffer(element)
-    
+
       do {
-          if (pathList.apply(col).size < range) {
-              val neighbours = getNeighbours(alreadyVisited.toList)
-    
-              if (neighbours.size == 0) {
-                  if (pathList.last == pathList(col))
-                      pathList = pathList.+=(ListBuffer.empty[(Int, Int)])
-                  col += 1
-                  alreadyVisited = ListBuffer()
-              }
-              else {
-                  for (n <- neighbours) toVisit.push(n)
-    
-                  // copy current List if more than one neighbour was found
-                  if (pathList.apply(col).size != 0)
-                      for (t <- 1 until neighbours.size)
-                          pathList.insert(col, pathList.apply(col))
-              }
+        if (pathList.apply(col).size <= range) {
+          val neighbours = getNeighbours(element, alreadyVisited.toList, g.gameField)
+
+          if (neighbours.size == 0) {
+            if (pathList.last == pathList(col))
+              pathList = pathList.+=(ListBuffer.empty[(Int, Int)])
+            col += 1
+            alreadyVisited = ListBuffer()
           }
           else {
-              if (pathList.last == pathList(col))
-                  pathList = pathList.+=(ListBuffer.empty[(Int, Int)])
-    
-              alreadyVisited = ListBuffer()
-              col += 1
+            for (n <- neighbours) toVisit.push(n)
+
+            // copy current List if more than one neighbour was found
+            if (pathList.apply(col).size != 0)
+              for (t <- 1 until neighbours.size)
+                pathList.insert(col, pathList.apply(col))
           }
-    
-          element = toVisit.pop
-          pathList(col) = pathList(col) ++ ListBuffer(element)
-          alreadyVisited.+=(element)
-    
+        }
+        else {
+          if (pathList.last == pathList(col)) {
+            pathList = pathList.+=(ListBuffer.empty[(Int, Int)])
+          } else {
+            alreadyVisited = ListBuffer()
+          }
+          col += 1
+        }
+
+        element = toVisit.pop
+        pathList(col) = pathList(col) ++ ListBuffer(element)
+        alreadyVisited.+=(element)
+
       } while (!toVisit.isEmpty)
-    
+      
+      println(pathList)
       pathList.toList.flatten.distinct
     }
     
     // Input must be (y,x). map needs coords in (y,x)
     // result is a List of valid coordinates for the array (y,x)
-    def getNeighbours(alreadyVisited : List[(Int, Int)]) = {
+    // TODO: This function must check if the neighbour is not a
+    // wall!!
+    def getNeighbours(startPoint : (Int,Int), alreadyVisited : List[(Int, Int)], gF : GameField) = {
         // von Neumann neighborhood, East, South, West, North
         val neighbours = 
-          ListBuffer(tuple.rightOf) ++
-          ListBuffer(tuple.belowOf) ++
-          ListBuffer(tuple.leftOf) ++
-          ListBuffer(tuple.aboveOf)
+          ListBuffer(startPoint.rightOf) ++
+          ListBuffer(startPoint.belowOf) ++
+          ListBuffer(startPoint.leftOf) ++
+          ListBuffer(startPoint.aboveOf)
   
         for (
             n <- neighbours.toList;
-            if (!alreadyVisited.contains(n))
+            if (!alreadyVisited.contains(n));
+            if (!gF.gameFieldCells(n).containsWall)
         ) yield n
     }      
 	}

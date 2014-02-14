2c6492031e6af80a081ae94dd9f44c4a1b44e9bc
Lars Eckervogt <laeckerv@htwg-konstanz.de>
2014-02-14 22:27:00 +0100

    Better use sbt project picker :-)

--- gui/src/main/scala/view/main/Main.scala
+++ gui/src/main/scala/view/main/Main.scala
@@ -2,18 +2,25 @@ package view.main
 
 import controller.Communication
 import model.ViewModel
-import view.tui.Tui
 import view.gui.Gui
+import view.tui.Tui
+
+object Gui {
+
+  def main(args: Array[String]) {
+    val model = new ViewModel
+    val controller = new Communication(model)
+    val gui = new Gui(model, controller)
+    gui.visible = true
+  }
+}
 
 object MainGui {
 
   def main(args: Array[String]) {
     val model = new ViewModel
     val controller = new Communication(model)
-
-    val gui = new Gui(model, controller)
     val tui = new Tui(model, controller)
-    gui.visible = true
     tui.show
   }
 }

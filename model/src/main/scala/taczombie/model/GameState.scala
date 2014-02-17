d41dac5c3b93172a6098abc229c5b74fcf84c2d9
Stefan Junker <mail@stefanjunker.de>
2014-02-17 03:28:15 +0100

    Game: fix GameOver handling

--- model/src/main/scala/taczombie/model/GameState.scala
+++ model/src/main/scala/taczombie/model/GameState.scala
@@ -2,5 +2,5 @@ package taczombie.model
 
 object GameState extends Enumeration {
 	type GameState = Value
-	val Lose, Win, InGame, NeedPlayerSwitch, NeedTokenSwitch, GameOver = Value
+	val GameOver, Win, InGame, NeedPlayerSwitch, NeedTokenSwitch = Value
 }
